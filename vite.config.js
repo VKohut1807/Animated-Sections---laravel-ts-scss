8f3e41eb6c111732653a1a36a36737dfbd63f0f5
VKohut1807 <vasylkohut1991@gmail.com>
2025-11-22 20:58:38 +0000

    fixed:vite

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,31 @@ import laravel from "laravel-vite-plugin";
 import path from "path";
 
 const filesStyle = [
-  // './resources/scss/global/index.scss',
-  // './resources/scss/layouts/section-13.scss'
+  './resources/scss/global/index.scss',
+  './resources/scss/layouts/home.scss',
+  './resources/scss/layouts/section-1.scss',
+  './resources/scss/layouts/section-2.scss',
+  './resources/scss/layouts/section-3.scss',
+  './resources/scss/layouts/section-4.scss',
+  './resources/scss/layouts/section-5.scss',
+  './resources/scss/layouts/section-6.scss',
+  './resources/scss/layouts/section-7.scss',
+  './resources/scss/layouts/section-8.scss',
+  './resources/scss/layouts/section-9.scss',
+  './resources/scss/layouts/section-10.scss',
+  './resources/scss/layouts/section-11.scss',
+  './resources/scss/layouts/section-12.scss',
+  './resources/scss/layouts/section-13.scss',
+  './resources/scss/layouts/section-14.scss',
 ];
-const filesTs = [];
+const filesTs = [
+  './resources/ts/components/navigationDrawers.ts',
+  './resources/ts/partials/breadcrumbs.ts',
+  './resources/ts/layouts/section-2.ts',
+  './resources/ts/layouts/section-7.ts',
+  './resources/ts/layouts/section-8.ts',
+  './resources/ts/layouts/section-11.ts',
+];
 
 export default defineConfig({
   plugins: [
