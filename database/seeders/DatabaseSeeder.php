988d71804e1628cf27b961b4804bd01f0c388af5
VKohut1807 <vasylkohut1991@gmail.com>
2025-11-25 10:49:21 +0000

    chore:add-vendor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-<<<<<<< HEAD
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-=======
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
->>>>>>> 1d53d9c (feat:add-docker)
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-<<<<<<< HEAD
-=======
-    use WithoutModelEvents;
-
->>>>>>> 1d53d9c (feat:add-docker)
     /**
      * Seed the application's database.
      */
