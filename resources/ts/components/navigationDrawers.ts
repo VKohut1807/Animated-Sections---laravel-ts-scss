417b33d61eb801d4493a461d6f0e601bb4ecaebf
VKohut1807 <vasylkohut1991@gmail.com>
2025-12-09 20:09:29 +0000

    chore:change-mixin-breakpoints--fix-nav-drawers

--- resources/ts/components/navigationDrawers.ts
+++ resources/ts/components/navigationDrawers.ts
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     showSectionsNr2(
         "[data-background-window]",
-        "[data-nav-drawers-views] [data-click]",
+        "[data-nav-drawers-views] [data-trigger]",
         "[data-nav-drawers-views]",
-        "[data-nav-drawers-views] [data-name]",
+        "[data-nav-drawers-views] [data-section-name]",
         "[data-nav-drawers-image]"
     );
 });
@@ -22,11 +22,11 @@ function showSectionsNr2(
     const image = document.querySelector(imageSelector) as HTMLImageElement;
 
     let currentActive: HTMLElement | null = null;
-    let tappedElement: HTMLElement | null = null;
 
     function showImage(section: HTMLElement) {
-        const link = section.dataset.name;
+        const link = section.dataset.sectionName;
         if (!image || !link) return;
+
 
         if (currentActive && currentActive !== section) {
             currentActive.classList.remove("active");
@@ -41,44 +41,20 @@ function showSectionsNr2(
     }
 
     function hideImage() {
-        if (!image) return;
+        if (!image || !currentActive) return;
 
         image.classList.remove("active");
-
-        if (currentActive) {
-            currentActive.classList.remove("active");
-            currentActive = null;
-        }
+        currentActive.classList.remove("active");
+        currentActive = null;
     }
 
     sections.forEach(section => {
-        section.addEventListener("mouseover", () => showImage(section));
-        section.addEventListener("mouseout", hideImage);
+        section.addEventListener('touchstart', () => showImage(section));
+        section.addEventListener('touchend', hideImage);
+        section.addEventListener('touchcancel', hideImage);
 
-        section.addEventListener("click", () => {
-            const href = section.getAttribute("href");
-            if (href) window.location.href = href;
-        });
-
-        section.addEventListener("touchstart", (e) => {
-            const href = section.getAttribute("href");
-
-            if (tappedElement !== section) {
-                e.preventDefault();
-                tappedElement = section;
-                showImage(section);
-
-                setTimeout(() => {
-                    tappedElement = null;
-                }, 600);
-
-            } else {
-                if (href) {
-                    hideImage();
-                    window.location.href = href;
-                }
-            }
-        });
+        section.addEventListener('mouseover', () => showImage(section));
+        section.addEventListener('mouseout', hideImage);
     });
 
     triggerElement?.addEventListener("click", () => {
--- resources/views/components/navigationDrawers.blade.php
+++ resources/views/components/navigationDrawers.blade.php
@@ -1,11 +1,14 @@
 <div data-nav-drawers-views class="sections">
-    <div class="hook-open" data-click>
+    <div class="hook-open" data-trigger>
         @svgPublic('/svg/section-2/open.svg')
     </div>
     <div class="quick-view">
         @foreach ($routesConfig as $key => $value)
-            <div data-name="{{ $value['view-name'] }}" href="{{ route($value['route-name']) }}" class="page-view">
+            <div data-section-name="{{ $value['view-name'] }}" class="page-view">
                 <h5>{{ $value['view-name'] }}</h5>
+                <a href="{{ route($value['route-name']) }}" class="page-link">
+                    @svgPublic('/svg/section-2/arrow-right-icon.svg')
+                </a>
             </div>
         @endforeach
     </div>
